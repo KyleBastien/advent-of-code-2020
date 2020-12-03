e06928361d8d60f1c40991b96f05a38ab0c64aed
Kyle Bastien <kbastien@microsoft.com>
2020-12-03 11:22:53 -0800

    Adding day 3 solutions

---
+++ day-3/puzzle-5.ts
@@ -0,0 +1,32 @@
+import { TREE, treeMap } from './map';
+
+const X_SLOPE = 3;
+const Y_SLOPE = 1;
+
+export const puzzle5 = () => {
+  return calculateTreesGivenSlope(treeMap, X_SLOPE, Y_SLOPE);
+};
+
+export const calculateTreesGivenSlope = (
+  treeMap: string[],
+  slopeX: number,
+  slopeY: number
+) => {
+  const width = treeMap[0].length;
+  const height = treeMap.length;
+  let x = 0;
+  let y = 0;
+  let treeCount = 0;
+  while (y < height) {
+    if (treeMap[y][x] === TREE) {
+      treeCount++;
+    }
+    x = (x + slopeX) % width;
+    y = y + slopeY;
+  }
+  return treeCount;
+};
+
+if (process.argv[1].indexOf('puzzle-5') > -1) {
+  console.log(puzzle5());
+}
---
+++ day-3/puzzle-6.ts
@@ -0,0 +1,24 @@
+import { treeMap } from './map';
+import { calculateTreesGivenSlope } from './puzzle-5';
+
+export const puzzle6 = () => {
+  const slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ];
+
+  let result = 1;
+  for (const slope of slopes) {
+    const trees = calculateTreesGivenSlope(treeMap, slope[0], slope[1]);
+    result = result * trees;
+  }
+
+  return result;
+};
+
+if (process.argv[1].indexOf('puzzle-6') > -1) {
+  console.log(puzzle6());
+}
