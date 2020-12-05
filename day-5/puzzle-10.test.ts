600d8a70bbc597148d15457f54c53af2d4cbc80c
Kyle Bastien <mcnuttkyle93@gmail.com>
2020-12-05 15:35:35 -0800

    Solving Day 5 (#2)

---
+++ day-5/puzzle-10.test.ts
@@ -0,0 +1,7 @@
+import { puzzle10 } from './puzzle-10';
+
+describe('Puzzle 10', () => {
+  it('should output 617 given the standard input', () => {
+    expect(puzzle10()).toEqual(617);
+  });
+});
