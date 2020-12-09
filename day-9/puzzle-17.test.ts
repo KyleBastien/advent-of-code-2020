c9b5a4a04b083b497c7ed0c8b24e0857d9ef0702
Kyle Bastien <kbastien@microsoft.com>
2020-12-08 21:42:57 -0800

    Solving Day 9

---
+++ day-9/puzzle-17.test.ts
@@ -0,0 +1,7 @@
+import { puzzle17 } from './puzzle-17';
+
+describe('Puzzle 17', () => {
+  it('should output 22406676 given the standard input', () => {
+    expect(puzzle17()).toEqual(22406676);
+  });
+});
---
+++ day-9/puzzle-18.test.ts
@@ -0,0 +1,7 @@
+import { puzzle18 } from './puzzle-18';
+
+describe('Puzzle 18', () => {
+  it('should output 2942387 given the standard input', () => {
+    expect(puzzle18()).toEqual(2942387);
+  });
+});
