4f9112fd698a98e789f235d8ab56d3116cb43757
Kyle Bastien <mcnuttkyle93@gmail.com>
2020-12-09 21:52:35 -0800

    Solving Day 10 (#7)

---
+++ day-10/puzzle-19.test.ts
@@ -0,0 +1,7 @@
+import { puzzle19 } from './puzzle-19';
+
+describe('Puzzle 19', () => {
+  it('should output 3034 given the standard input', () => {
+    expect(puzzle19()).toEqual(3034);
+  });
+});
---
+++ day-10/puzzle-20.test.ts
@@ -0,0 +1,7 @@
+import { puzzle20 } from './puzzle-20';
+
+describe('Puzzle 20', () => {
+  it('should output 259172170858496 given the standard input', () => {
+    expect(puzzle20()).toEqual(259172170858496);
+  });
+});
