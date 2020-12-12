d6bc31174b17f73095e683fa26e78cd371daef94
Kyle Bastien <kbastien@microsoft.com>
2020-12-12 15:20:36 -0800

    Solving Day 12

---
+++ day-12/puzzle-23.test.ts
@@ -0,0 +1,7 @@
+import { puzzle23 } from './puzzle-23';
+
+describe('Puzzle 23', () => {
+  it('should output 2280 given the standard input', () => {
+    expect(puzzle23()).toEqual(2280);
+  });
+});
