477e5b29531a52b03edf29f39c5b8b78adc98f53
Kyle Bastien <kbastien@microsoft.com>
2020-12-13 21:55:51 -0800

    Solving Day 14

---
+++ day-14/puzzle-27.test.ts
@@ -0,0 +1,7 @@
+import { puzzle27 } from './puzzle-27';
+
+describe('Puzzle 27', () => {
+  it('should output 3059488894985 given the standard input', () => {
+    expect(puzzle27()).toEqual(3059488894985);
+  });
+});
---
+++ day-14/puzzle-28.test.ts
@@ -0,0 +1,7 @@
+import { puzzle28 } from './puzzle-28';
+
+describe('Puzzle 28', () => {
+  it('should output 2900994392308 given the standard input', () => {
+    expect(puzzle28()).toEqual(2900994392308);
+  });
+});
