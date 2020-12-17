b35de21aef4c0400f4e1b8857446397ebecf982d
Kyle Bastien <mcnuttkyle93@gmail.com>
2020-12-16 22:07:04 -0800

    Solving Day 17 (#14)

---
+++ day-17/puzzle-33.test.ts
@@ -0,0 +1,7 @@
+import { puzzle33 } from './puzzle-33';
+
+describe('Puzzle 33', () => {
+  it('should output 386 given the standard input', () => {
+    expect(puzzle33()).toEqual(386);
+  });
+});
---
+++ day-17/puzzle-34.test.ts
@@ -0,0 +1,7 @@
+import { puzzle34 } from './puzzle-34';
+
+describe('Puzzle 34', () => {
+  it('should output 2276 given the standard input', () => {
+    expect(puzzle34()).toEqual(2276);
+  });
+});
