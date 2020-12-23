969f96e6f820cd64e1182adc82934fda726f8b85
Kyle Bastien <kbastien@microsoft.com>
2020-12-22 22:55:20 -0800

    Fixing puzzle 45 test

--- day-23/puzzle-45.test.ts
+++ day-23/puzzle-45.test.ts
@@ -2,6 +2,6 @@ import { puzzle45 } from './puzzle-45';
 
 describe('Puzzle 45', () => {
   it('should output 32658947 given the standard input', () => {
-    expect(puzzle45()).toEqual(32658947);
+    expect(puzzle45()).toEqual('32658947');
   });
 });
