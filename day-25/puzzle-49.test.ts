d1b65c86b2a910bde595bf5f7e78fa6876959a7e
Kyle Bastien <kbastien@microsoft.com>
2020-12-24 21:37:42 -0800

    Solving Day 25

---
+++ day-25/puzzle-49.test.ts
@@ -0,0 +1,7 @@
+import { puzzle49 } from './puzzle-49';
+
+describe('Puzzle 49', () => {
+  it('should output 16881444 given the standard input', () => {
+    expect(puzzle49()).toEqual(16881444);
+  });
+});
